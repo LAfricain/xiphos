f569280e69259f52ebde3f5368facf072aa7d2df
lafricain79 <lafricain79@gmail.com>
2026-08-17 12:16:48 +0200

    Update outdated FSF postal address in license headers
    Replace the obsolete '51 Franklin St, Fifth Floor, Boston, MA
    02110-1301, USA' address with the FSF-recommended pointer to
    <https://www.gnu.org/licenses/>, per issue #894 item 7.

--- src/backend/gs_stringmgr.cpp
+++ src/backend/gs_stringmgr.cpp
@@ -19,8 +19,7 @@
  * GNU Library General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, U.
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "gs_stringmgr.h"
